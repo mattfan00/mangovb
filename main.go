5b8a8067e8e911b10608973d1bc21cdf1e72f5bd
Matthew Fan <kungfumo@gmail.com>
2022-12-13 20:57:33 -0500

    initial version of parsing nyurban rows

--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
+type Event struct {
+	Name        string
+	Location    string
+	StartDate   time.Time
+	StartTime   string
+	EndTime     string
+	SkillLevel  string
+	Price       float64
+	IsAvailable bool
+	SpotsLeft   int
+	Url         string
+	Source      string
+}
+
 func main() {
 	c := colly.NewCollector()
 
+	const source = "NY Urban"
+	const url = "https://www.nyurban.com/open-play-registration-vb/?id=35&gametypeid=1&filter_id=1"
+
 	c.OnHTML("div.time_schedule_table", func(tableDiv *colly.HTMLElement) {
+		events := []Event{}
+
 		location := strings.TrimSuffix(tableDiv.ChildText("h3 > span"), ":")
 		fmt.Println(location)
 
@@ -19,11 +42,77 @@ func main() {
 				return
 			}
 
-			fmt.Printf("%s\t", row.ChildText("td:nth-child(2)"))
-			fmt.Printf("%s\t", row.ChildText("td:nth-child(3)"))
-			fmt.Printf("%s\t", row.ChildText("td:nth-child(4)"))
-			fmt.Printf("%s\t", row.ChildText("td:nth-child(5)"))
-			fmt.Printf("%s\n", row.ChildText("td:nth-child(6)"))
+			event := Event{}
+
+			event.Name = row.ChildText("td:nth-child(3)")
+			event.Location = location
+			event.Url = url
+			event.Source = source
+
+			price, err := strconv.ParseFloat(row.ChildText("td:nth-child(5)"), 64)
+			if err != nil {
+				log.Println(err)
+			} else {
+				event.Price = price
+			}
+
+			parsedStartDate, err := time.Parse("Mon 01/02", row.ChildText("td:nth-child(2)"))
+			if err != nil {
+				log.Println(err)
+			} else {
+				currentTime := time.Now()
+				eventYear := currentTime.Year()
+
+				// if the parsed month is less than the current month, that means the
+				// event takes place in the next year
+				if parsedStartDate.Month() < currentTime.Month() {
+					eventYear = currentTime.Year() + 1
+				}
+
+				event.StartDate = time.Date(
+					eventYear,
+					parsedStartDate.Month(),
+					parsedStartDate.Day(),
+					0, 0, 0, 0,
+					parsedStartDate.Location(),
+				)
+			}
+
+			rawTimesSplit := strings.Split(row.ChildText("td:nth-child(4)"), "-")
+			rawStartTime := strings.Trim(rawTimesSplit[0], " ")
+			rawEndTime := strings.Trim(rawTimesSplit[1], " ")
+
+			parsedStartTime, err := time.Parse("3:04 pm", rawStartTime)
+			if err != nil {
+				log.Println(err)
+			} else {
+				event.StartTime = parsedStartTime.Format("15:04")
+			}
+
+			parsedEndTime, err := time.Parse("3:04 pm", rawEndTime)
+			if err != nil {
+				log.Println(err)
+			} else {
+				event.EndTime = parsedEndTime.Format("15:04")
+			}
+
+			rawAvail := row.ChildText("td:nth-child(6)")
+			if rawAvail == "Yes" {
+				event.IsAvailable = true
+			} else if rawAvail == "Sold Out" {
+				event.IsAvailable = false
+			} else if strings.Contains(strings.ToLower(rawAvail), "space") {
+				re := regexp.MustCompile("[0-9]+")
+				rawSpotsLeft := re.FindString(rawAvail)
+
+				event.IsAvailable = true
+				event.SpotsLeft, err = strconv.Atoi(rawSpotsLeft)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+
+			events = append(events, event)
 		})
 	})
 
@@ -32,5 +121,5 @@ func main() {
 	})
 
 	c.UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1"
-	c.Visit("https://www.nyurban.com/open-play-registration-vb/?id=35&gametypeid=1&filter_id=1")
+	c.Visit(url)
 }
