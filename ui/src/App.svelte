19b7416ae3b3c578a7107fcad0511b72a0f3ca3c
Matthew Fan <kungfumo@gmail.com>
2023-02-03 22:21:01 -0500

    added main grid

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import dayjs from "dayjs";
     import type { Event } from "./types";
 
     let events: Event[] = [];
@@ -13,12 +14,28 @@
 
 
 <main class="flex justify-center">
-    <div class="max-w-2xl w-full mt-20">
-        {#each events as event}
-            <div class="flex justify-between">
+    <div class="max-w-2xl w-full mt-20 mb-40">
+        <div class="main-grid">
+            <div class="main-grid-header">Source</div>
+            <div class="main-grid-header">Name</div>
+            <div class="main-grid-header">Location</div>
+            <div class="main-grid-header">Spots</div>
+            <div class="main-grid-header">Start time</div>
+            {#each events as event}
+                <div>{event.sourceId}</div>
                 <div>{event.name}</div>
-                <div>{event.startTime}</div>
-            </div>
-        {/each}
+                <div>{event.location}</div>
+                <div>
+                    {#if !event.isAvailable}
+                        Filled
+                    {:else if  event.isAvailable && event.spotsLeft == 0}
+                        Avail
+                    {:else}
+                        {event.spotsLeft}
+                    {/if}
+                </div>
+                <div>{dayjs(event.startTime).format("MMM DD h:mm A")}</div>
+            {/each}
+        </div>
     </div>
 </main>
--- ui/src/types.ts
+++ ui/src/types.ts
@@ -2,13 +2,13 @@ export interface Event {
     id: string;
     name: string;
     location: string;
-    startTime: Date;
-    endTime: Date;
+    startTime: string;
+    endTime: string;
     skillLevel: number;
     price: number;
     isAvailable: boolean;
     spotsLeft: number;
     url: string;
     sourceId: number;
-    updatedOn: Date;
+    updatedOn: string;
 }
