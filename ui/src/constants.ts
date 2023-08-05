a7528ea7c0743eca55c950f779ef67e6df4f1aa2
Matthew Fan <kungfumo@gmail.com>
2023-08-05 00:18:55 -0400

    prod ui uses prod api

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = process.env.NODE_ENV === "prod" ? "https://api.mangovb.com" : "http://localhost:8080";
 export const API_FILTERS_URL = `${API_BASE_URL}/filters`;
 export const API_EVENTS_URL = `${API_BASE_URL}/events`;
